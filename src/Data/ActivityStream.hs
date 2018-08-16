030f0faf398f8189fe318029203f667fa849df2a
Jose Prous <joseprous@gmail.com>
2018-08-16 16:34:48 -0400

    first attempt at ActivityStream vocabulary

---
+++ src/Data/ActivityStream.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+module Data.ActivityStream where
