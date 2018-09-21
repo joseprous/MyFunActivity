c7b6b7644c4db6396f9c289ebe7108503ea5a03d
Jose Prous <joseprous@gmail.com>
2018-09-21 15:24:34 -0400

    travis in loop

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo "begin deploy.sh"
 cp .stack-work/install/x86_64-linux/lts-12.0/8.4.3/bin/MyFunActivity MyFunActivity
 echo "after copy"
-git add MyFunActivity
-git commit -m "add release"
+#git add MyFunActivity
+#git commit -m "add release"
 echo "end deploy.sh"
