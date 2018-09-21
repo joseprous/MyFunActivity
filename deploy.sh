d882108efb6422582ae22b02da3f5d52aec5f823
Jose Prous <joseprous@gmail.com>
2018-09-21 15:15:04 -0400

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git config --global user.email "${GIT_EMAIL}"
-git config --global user.name "${GIT_NAME}"
+echo "begin deploy.sh"
 cp .stack-work/install/x86_64-linux/lts-12.0/8.4.3/bin/MyFunActivity MyFunActivity
+echo "after copy"
 git add MyFunActivity
 git commit -m "add release"
+echo "end deploy.sh"
