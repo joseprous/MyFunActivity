c6a6e73fe189be64f773f397f80b5e975d5028d4
Jose Prous <joseprous@gmail.com>
2018-09-21 15:33:28 -0400

    new branch for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "begin deploy.sh"
+git checkout -b deploy
 cp .stack-work/install/x86_64-linux/lts-12.0/8.4.3/bin/MyFunActivity MyFunActivity
 echo "after copy"
-#git add MyFunActivity
-#git commit -m "add release"
+git add MyFunActivity
+git commit -m "add release"
 echo "end deploy.sh"
